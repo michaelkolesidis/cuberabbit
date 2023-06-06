7a5ab65657239992f3717394151e6d9bcca852e1
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-06-07 01:53:59 +0300

    vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // root: "src/",
+  // publicDir: "../public/",
+  // base: "./",
   server: {
     host: true,
     open: false,
   },
   build: {
-    outDir: "../dist",
+    outDir: "./dist",
     emptyOutDir: true,
     sourcemap: false,
     minify: "esbuild",
