7cfe1e3e303578421acf46b3785da914ff91c167
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-06-06 20:23:49 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Untitled Bunny Game
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Untitled Bunny Game</h1>
+      <img src="./bunny.png" />
+    </>
+  );
+}
+
+export default App;
